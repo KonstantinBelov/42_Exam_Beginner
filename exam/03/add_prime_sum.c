f98076b73ce25894edc15ea9e0ebd94cc3864de8
Konstantin Belov <kbelov@e1z2r5p33.42.us.org>
2019-03-02 18:54:01 -0800

    add_prime_summ

--- exam/03/add_prime_sum.c
+++ exam/03/add_prime_sum.c
@@ -6,55 +6,82 @@
 /*   By: kbelov <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/31 01:19:27 by kbelov            #+#    #+#             */
-/*   Updated: 2019/01/31 19:06:11 by kbelov           ###   ########.fr       */
+/*   Updated: 2019/03/02 18:47:21 by kbelov           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int			ft_atoi(const char *str)
+int			ft_atoi_pos(const char *str)
 {
-	int		result;
-	int		sign;
+	int		num;
 	int		i;
 
-	result = 0;
-	sign = 1;
+	num = 0;
 	i = 0;
-	while (str[i] <= 32)
+	while (str[i] <= 32 && str[i])
 		i++;
 	if (str[i] == '+' || str[i] == '-')
 	{
 		if (str[i] == '-')
-			sign = -1;
+			return (0);
 		i++;
 	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		result *= 10;
-		result += str[i] - '0';
+		num *= 10;
+		num += str[i] - '0';
 		i++;
 	}
-	return (result * sign);
+	return (num);
 }
 
-int			ft_is_prime(int num)
+int			ft_is_prime(int n)
 {
 	int		i;
-	
-	while (num
+
+	i = 2;
+	while (i < n)
+	{
+		if ((n % i) == 0)
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-int			ft_prime_sum(int num)
+void		ft_putnum(int num)
 {
-	
+	char	c;
+
+	if (num > 9)
+		ft_putnum(num / 10);
+	c = num % 10 + '0';
+	write(1, &c, 1);
+}
+
+void		ft_prime_sum(int num)
+{
+	int		n;
+	int		sum;
+
+	n = 2;
+	sum = 0;
+	while (n <= num)
+	{
+		if (ft_is_prime(n) == 1)
+			sum += n;
+		n++;
+	}
+	ft_putnum(sum);
 }
 
 int			main(int ac, char *av[])
 {
-	int		num;
-
-	if (ac == 2 && av[1] >= '0' && av[1] <= '9')
-	{
-		num = ft_atoi(av[1]);
-		ft_prime_sum(num);
+	if (ac == 2 && ft_atoi_pos(av[1]) > 0)
+		ft_prime_sum(ft_atoi_pos(av[1]));
+	else
+		write(1, "0", 1);
+	write(1, "\n", 1);
+	return (0);
+}
