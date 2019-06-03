5633ba916a541930315a37a640351a925b0cf0f6
Konstantin Belov <kbelov@e1z2r4p21.42.us.org>
2019-06-02 23:28:38 -0700

    epur_str

--- 03/3-0-epur_str/epur_str.c
+++ 03/3-0-epur_str/epur_str.c
@@ -5,19 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kbelov <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/03/02 19:05:04 by kbelov            #+#    #+#             */
-/*   Updated: 2019/03/02 22:45:49 by kbelov           ###   ########.fr       */
+/*   Created: 2019/06/02 23:02:01 by kbelov            #+#    #+#             */
+/*   Updated: 2019/06/02 23:26:25 by kbelov           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int		ft_isblanc(char c)
-{
-	if (c == ' ' || c == '\t')
-		return (1);
-	return (0);
-}
+#define IS_BLANK(c) (c == ' ' || c == '\t')
 
 void	ft_putchar(char c)
 {
@@ -26,13 +21,14 @@ void	ft_putchar(char c)
 
 void	ft_epur_str(char *s)
 {
-	while (ft_isblanc(*s) == 1 && *s != '\0')
+	while (IS_BLANK(*s))
 		s++;
 	while (*s != '\0')
 	{
-		if (ft_isblanc(*s) == 0 ||
-				((ft_isblanc(*(s + 1)) == 0) && *(s + 1) != '\0'))
+		if (!IS_BLANK(*s))
 			ft_putchar(*s);
+		else if (IS_BLANK(*s) && !IS_BLANK(*(s + 1)) && *(s + 1) != '\0')
+			ft_putchar(' ');
 		s++;
 	}
 }
